12466669a8c62db7fd79b90d1a8201b5b73cae71
angshums <angshuman2020@gmail.com>
2016-06-28 21:31:30 +0530

    Challenge 01

--- challenge01/eudyptula01.c
+++ challenge01/eudyptula01.c
@@ -1,3 +1,4 @@
+#define DEBUG
 #include <linux/module.h>
 #include <linux/kernel.h>
 
@@ -6,13 +7,14 @@
 /* initialize the LKM */
 int init_module(void)
 {
-	printk(KERN_INFO "init_module...\n");
+	pr_debug("init_module...\n");
 	return 0;
 }
 
 /* cleanup - undo whatever init_module did */
 void cleanup_module(void)
 {
-	printk(KERN_INFO "cleanup_module...\n");
+	pr_debug("cleanup_module...\n");
 }
 
+MODULE_LICENSE("GPL");
